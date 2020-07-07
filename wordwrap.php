311655f8d966cc3c4b4593a79de47a92e2b8522b
batistajon <batista.jonathas@gmail.com>
2020-07-07 18:45:45 -0300

    criação do exercício wordwrap

---
+++ wordwrap.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wordwrap</title>
+</head>
+<body>
+    <?php
+        $t = "Aqui temos um texto gigante para mostrar o funcionamento da função wordwrap";
+    ?>
+</body>
+</html>
