a591625c53fe4ae4e3d9184fba092e7d24822c3f
batistajon <batista.jonathas@gmail.com>
2020-07-07 17:33:50 -0300

    php teste

---
+++ printf.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Função printf</title>
+</head>
+<body>
+    <?php
+        $p = leite;
+        $pr = 4.5;
+
+        echo "o $p custa $pr";
+    ?>
+</body>
+</html>
