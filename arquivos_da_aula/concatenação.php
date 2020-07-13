506ea669bb03f134c52be248ca831fca8e45b073
batistajon <batista.jonathas@gmail.com>
2020-07-13 17:26:47 -0300

    deletando e adicionando arquivo pelo vscode

---
+++ arquivos_da_aula/concatenação.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concatenação</title>
+</head>
+<body>
+    <?php
+        echo 'Olá Jon, me vende um pacote de café aí?'
+    ?>
+</body>
+</html>
